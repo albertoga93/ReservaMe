8f6ef30f6ad8793f27e2c5917f4be5d9706ce725
Alberto García Carrasco <garciacarrascoalberto@gmail.com>
2026-09-09 08:59:27 +0200

    se hacel el repository de cliente

---
+++ src/main/java/org/alberto/reservame/cliente/ClienteRepository.java
@@ -0,0 +1,11 @@
+package org.alberto.reservame.cliente;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+
+    Optional<Cliente> findByTelefono(String telefono);
+    
+}
